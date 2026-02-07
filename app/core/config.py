1d7915e68f6cccd6217e901a9a2fc8696fa9a6a2
rishav9966 <39216235+rishav9966@users.noreply.github.com>
2026-02-08 01:44:44 +0530

    Changes for  default DB Connection URL

--- app/core/config.py
+++ app/core/config.py
@@ -12,7 +12,7 @@ class Settings:
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 
 settings = Settings()
